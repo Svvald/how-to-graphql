e06c3a8e9de1e793b0149d99401f42b07c3b4077
Nikita Chistousov <nc@svvald.me>
2021-08-16 02:40:41 +0500

    graphql-node: complete chapter 3

--- server/index.js
+++ server/index.js
@@ -1,17 +1,6 @@
+const fs = require('fs');
+const path = require('path');
 const {ApolloServer} = require('apollo-server');
-
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-    
-  type Link {
-    id: ID!
-    description: String!
-    url: String!      
-  }
-`;
 
 let links = [
   {
@@ -25,17 +14,66 @@ const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews clone`,
     feed: () => links,
+    link: (parent, args) => {
+      return links.find(link => link.id === args.id) || null;
+    }
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
+  Mutation: {
+    post: (parent, args) => {
+      let idCount = links.length;
+
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+
+      links = links.concat(link);
+
+      return link;
+    },
+    updateLink: (parent, args) => {
+      let updatedLink;
+
+      links = links.map(link => {
+        if (link.id === args.id) {
+          link = {
+            ...link,
+            description: args.description || link.description,
+            url: args.url || link.url,
+          };
+
+          updatedLink = link;
+        }
+
+        return link;
+      });
+
+      return updatedLink || null;
+    },
+    deleteLink: (parent, args) => {
+      let deletedLink;
+
+      links = links.filter(link => {
+        if (link.id === args.id) {
+          deletedLink = link;
+          return false;
+        }
+
+        return true;
+      });
+
+      return deletedLink || null;
+    },
   },
 };
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8',
+  ),
   resolvers,
 });
 
-server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
+server.listen().then(({url}) => console.log(`Server is running on ${url}`));
